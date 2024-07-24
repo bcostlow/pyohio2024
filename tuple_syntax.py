de15bad8585dbcf69e124b0f7329d97bd5a6938c
Brian Costlow <brian.costlow@gmail.com>
2024-07-24 16:15:44 -0400

    Tuple syntax.

---
+++ tuple_syntax.py
@@ -0,0 +1,20 @@
+
+with_parens = (1, 2)
+
+print(type(with_parens))
+print(with_parens)
+
+
+no_parens = 1, 2
+
+print(type(no_parens))
+print(no_parens)
+
+comma_makes_the_tuple = 'really?',
+
+print(type(comma_makes_the_tuple))
+print(comma_makes_the_tuple)
+
+empty_tuple = ()
+print(type(empty_tuple))
+print(empty_tuple)
